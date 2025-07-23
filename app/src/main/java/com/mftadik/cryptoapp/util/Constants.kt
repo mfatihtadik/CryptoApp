569a6b1ae8fc76a88af5c9779b10f0ba8af27ce2
fatih <fatihtadk@gmail.com>
2025-07-23 14:25:15 +0300

    dependencies added.

---
+++ app/src/main/java/com/mftadik/cryptoapp/util/Constants.kt
@@ -0,0 +1,14 @@
+package com.mftadik.cryptoapp.util
+
+object Constants {
+
+    //https://raw.githubusercontent.com/atilsamancioglu/IA32-CryptoComposeData/refs/heads/main/cryptolist.json
+
+    //https://raw.githubusercontent.com/atilsamancioglu/IA32-CryptoComposeData/refs/heads/main/crypto.json
+
+    const val BASE_URL = "https://raw.githubusercontent.com/"
+    const val API_KEY = ""
+    const val CALL_ATTRIBUTES = "id, namei logo_url"
+
+
+}
